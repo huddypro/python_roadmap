aef16fcbf38c56e6b266f10e8fb9c34baf2e4464
huddypro <kikomekohudson@gmail.com>
2023-10-04 15:53:46 +0300

    list comprehension

--- list_comp.py
+++ list_comp.py
@@ -5,3 +5,22 @@ even_num = [num for num in numbers if num % 2 == 0]
         even_num.append(num)'''
 
 print(even_num)
+
+
+#exercise 2
+
+square = [x ** 2 for x in range(1, 10)]
+'''for x in range(1, 10):
+    sqr = x ** 2
+    square.append(sqr)'''
+
+print(square)
+
+#exercise 3
+
+colors = ['Red', 'Blue', 'Green', 'Black', 'White']
+sec_colors = [color.upper() for color in colors]
+'''for color in colors:
+    sec_colors.append(color.upper())'''
+
+print(sec_colors)
