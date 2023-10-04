d492e3dfe740f2a9debb62f09697e68c80f8349c
huddypro <kikomekohudson@gmail.com>
2023-10-04 15:28:26 +0300

    list comprehension

---
+++ list_comp.py
@@ -0,0 +1,7 @@
+numbers = [40, 45, 23, 87, 75, 78, 65, 46, 58, 76, 18, 94]
+even_num = [num for num in numbers if num % 2 == 0]
+'''for num in numbers:
+    if num % 2 == 0:
+        even_num.append(num)'''
+
+print(even_num)
