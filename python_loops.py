5f6d3a3a78d4b9069f3ee1bd83372205890b9e63
huddypro <kikomekohudson@gmail.com>
2023-10-12 12:49:40 +0300

    python loops

---
+++ python_loops.py
@@ -0,0 +1,9 @@
+favorites = ["fish", "irish", "rolex", "kameela", "software engineer"]
+
+for idx, items in enumerate(favorites):
+    print(idx , items)
+
+#count = 0
+#while count < len(favorites):
+ #   print("i like",favorites[count])
+  #  count += 1
